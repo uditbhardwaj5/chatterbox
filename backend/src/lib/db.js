fb3a67085ab72ed0ee02ae31580d4cfa417c4866
uditbhardwaj5 <uditbhardwaj804@gmail.com>
2026-02-06 20:46:16 +0530

    signup section code refactoring

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,6 +2,10 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
+    const { MONGO_URI } = process.env;
+    if (!MONGO_URI) {
+      throw new Error("MONGO_URI is not defined in environment variables");
+    }
     const conn =await mongoose.connect(process.env.MONGO_URI)
     console.log("MongoDB Connected:", conn.connection.host)
   } catch (error) {
--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,7 +1,13 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (userId, res) => {
-    const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+    const { JWT_SECRET } = process.env;
+    if (!JWT_SECRET) {
+        throw new Error("JWT_SECRET is not defined in environment variables");
+    }
+    
+    
+    const token = jwt.sign({ id: userId }, JWT_SECRET, {
         expiresIn: "7d",
     });
     res.cookie("jwt", token, {
