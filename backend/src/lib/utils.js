1b047550a3875563a75e8c77321b9e96c771421b
uditbhardwaj5 <uditbhardwaj804@gmail.com>
2026-02-27 01:17:58 +0530

    Fix cookie sameSite policy for cross-domain requests

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -14,7 +14,7 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
     httpOnly: true, // prevent XSS attacks: cross-site scripting
-    sameSite: "strict", // CSRF attacks
+    sameSite: ENV.NODE_ENV === "development" ? "lax" : "none", // Allow cross-domain cookies in production
     secure: ENV.NODE_ENV !== "development",
   });
 
